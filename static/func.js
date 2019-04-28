31ad2ade810c06a00e1c47bb72b98be2222dd6a5
The Hoff <11578521+xjarodx@users.noreply.github.com>
2019-04-28 10:56:50 -0400

    initial push

---
+++ static/func.js
@@ -0,0 +1,7 @@
+function openFileLoad(){
+    document.getElementById('seabird').style.visibility = 'visible';
+}
+
+function openFileLoad(){
+    document.getElementById('seabird').style.visibility = 'visible';
+}
