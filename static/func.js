499f45d928d9cd7b9d3c89f9a829e30c79d3a1e8
The Hoff <11578521+xjarodx@users.noreply.github.com>
2019-05-01 22:03:17 -0400

    css and js nightmares

--- static/func.js
+++ static/func.js
@@ -6,8 +6,12 @@ function openFileLoad() {
       x.style.visibility = "hidden";
     }
   } 
+  
+//var image =  document.getElementById("search");
 
-function gotIt() {
-  var q = document.getElementById("search");
-
-}
+//function gotIt() {
+//  if (image.getAttribute('src') == "/static/html_Images/birdIconWithText.png") {
+//      image.src = "/static/html_Images/Icon_Bird_512x512.png";}
+//  else {
+//      image.src = "/static/html_Images/birdIconWithText.png";}
+//}
